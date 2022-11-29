9c3926eaadd8e12acacddba13b730e05524deb1d
Breno Soares <brenosoares4303@gmail.com>
2022-11-29 17:02:03 -0300

    insereFila

--- Filas.c
+++ Filas.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Declaração dos tipos que serão usados:
 typedef int Item;
 typedef struct elemFila{
     Item item;
     struct elemFila *proximo;
 } ElemFila;
-typedef struct Fila{
+typedef struct{
     ElemFila *primeiro;
     ElemFila *ultimo;
+} Fila;
+
+// Declaração das Funções:
+void insereFila(Fila *fila, Item item){
+    ElemFila *aux;
+    aux = malloc(sizeof(ElemFila));
+    aux->proximo = NULL;
+
+    if(fila->primeiro == NULL){
+        fila->primeiro = aux;
+        fila->ultimo = aux;
+    } else{
+        fila->ultimo->proximo = aux;
+        fila->ultimo = aux;
+    }
+}
+
+void removeFila(){
+
+}
+
+void inicializaFila(){
+
+}
+
+int filaVazia(){
+
+}
+
+void liberaFila(){
+
 }
 
 int main (){
+    Fila fila;
 
     return 0;
 }
